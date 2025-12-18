a10f411b853a5d280ed6c79117a5a688a8c20053
Davod <davodnikoyii@gmail.com>
2025-12-18 14:10:35 +0100

    Add ExpensesController and view; update DB connection string
    Added ExpensesController with DI for FinanceAppContext and an index action. Created a placeholder Index.cshtml view. Updated the SQL Server connection string password in appsettings.json.

---
+++ MoneyMap/Controllers/ExpensesController.cs
@@ -0,0 +1,21 @@
+﻿using FinanceApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly FinanceAppContext _context;
+        public ExpensesController(FinanceAppContext conext)
+        {
+            _context = conext;
+        }
+
+        public IActionResult index()
+        {
+            return View();
+        }
+
+    }
+}
