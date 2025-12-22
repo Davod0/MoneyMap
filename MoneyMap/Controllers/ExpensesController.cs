6ae6ba657180466b4db83168c82f29d93a62024f
Davod <davodnikoyii@gmail.com>
2025-12-22 14:58:01 +0100

    Refactor ExpensesController to use IExpensesService DI
    Replaced direct DbContext usage in ExpensesController with IExpensesService for improved separation of concerns. Registered IExpensesService and its implementation in Program.cs for dependency injection. Updated controller actions to use service methods and made minor naming corrections.

--- MoneyMap/Controllers/ExpensesController.cs
+++ MoneyMap/Controllers/ExpensesController.cs
@@ -1,39 +1,37 @@
 ﻿using FinanceApp.Data;
+using FinanceApp.Data.Service;
 using FinanceApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
 
 namespace FinanceApp.Controllers
 {
     public class ExpensesController : Controller
     {
-        private readonly FinanceAppContext _context;
-        public ExpensesController(FinanceAppContext conext)
+        private readonly IExpensesService _expensesService;
+        public ExpensesController(IExpensesService expensesService)
         {
-            _context = conext;
+            _expensesService = expensesService;
         }
-
-        public async Task<IActionResult> index()
+        public async Task<IActionResult> Index()
         {
-            var expenses = await  _context.Expenses.ToListAsync();
+            var expenses = await _expensesService.GetAll();
             return View(expenses);
         }
-
         public IActionResult Create()
         {
             return View();
         }
-
         [HttpPost]
-        public async Task<ActionResult> Create(Expense expense)
+        public async Task<IActionResult> Create(Expense expense)
         {
             if (ModelState.IsValid)
             {
-                _context.Expenses.Add(expense);
-                await _context.SaveChangesAsync();
+                await _expensesService.Add(expense);
+
                 return RedirectToAction("Index");
             }
+
             return View(expense);
         }
     }
--- MoneyMap/Program.cs
+++ MoneyMap/Program.cs
@@ -1,16 +1,16 @@
 using FinanceApp.Data;
+using FinanceApp.Data.Service;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<FinanceAppContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+builder.Services.AddScoped<IExpensesService, ExpensesService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
